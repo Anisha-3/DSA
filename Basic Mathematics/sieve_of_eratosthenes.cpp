3ba0e987c2431cc2ec558a039c8dada21ba3ac3d
Anisha Asnani <anishaasnani2004@gmail.com>
2024-02-02 16:24:17 +0530

    commit

--- Basic Mathematics/sieve_of_eratosthenes.cpp
+++ Basic Mathematics/sieve_of_eratosthenes.cpp
@@ -6,7 +6,7 @@ vector<bool> Sieve(int n) {
     // create a sieve array of N size telling isPrime
     vector<bool> sieve(n + 1, true); // all are prime marked already
     sieve[0] = sieve[1] = false;
-    for(int i = 2; i <= n; i++) {
+    for(int i = 2; i*i <= n; i++) {  // optimisation 2
         if(sieve[i]) {            
             // int j = 2*i;
             int j = i*i; // optimisation 1: first unmarked number would be i*i, as others have been marked by 2 to (i - 1)
