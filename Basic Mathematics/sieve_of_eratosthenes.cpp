bbd5026f95835c453a1e33133fbc9f372328b7d1
Anisha Asnani <anishaasnani2004@gmail.com>
2024-02-03 23:25:28 +0530

    commit

--- Basic Mathematics/sieve_of_eratosthenes.cpp
+++ Basic Mathematics/sieve_of_eratosthenes.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 using namespace std;
 
 vector<bool> Sieve(int n) {
@@ -19,11 +20,47 @@ vector<bool> Sieve(int n) {
     return sieve;
 }
 
-int main() {
-    vector<bool>sieve = Sieve(25);
-    for(int i = 0; i <= 25; i++) {
+vector<bool> segSieve(int L, int R) {
+    // get me prime array, will use it to mark seg sieve
+    vector<bool> sieve = Sieve(sqrt(R));
+    vector<int> basePrimes;
+    for(int i = 0; i < sieve.size(); i++) {
         if(sieve[i]) {
-            cout << i << " ";
+            basePrimes.push_back(i);
+        }
+    }
+    vector<bool> segSieve(R-L+1, true);
+    if(L == 0 || L == 1) {
+        segSieve[L] = false;
+    }
+    for(auto prime: basePrimes) {
+        int first_mul = (L / prime) * prime;
+        if(first_mul < L) {
+            first_mul += prime;
+        }
+        int j = max(first_mul, prime*prime);
+        while(j <= R) {
+            segSieve[j - L] = false;
+            j += prime;
+        }
+    }
+    return segSieve;
+}
+
+int main() {
+    // vector<bool>sieve = Sieve(25);
+    // for(int i = 0; i <= 25; i++) {
+    //     if(sieve[i]) {
+    //         cout << i << " ";
+    //     }
+    // }
+
+    int L = 110;
+    int R = 130;
+    vector<bool>segmentedSieve = segSieve(L, R);
+    for(int i = 0; i < segmentedSieve.size(); i++) {
+        if(segmentedSieve[i]) {
+            cout << i + L << " ";
         }
     }
     return 0;
